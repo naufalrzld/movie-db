2ea9968062ddd7f61b7aa3541f57d73c06ee1fe5
Naufal <naufalrzld@gmail.com>
2023-09-08 13:34:44 +0700

    add unit test MainViewModel & DetailViewModel

---
+++ app/src/test/java/com/example/moviedb/ui/detail/DetailMovieViewModelTest.kt
@@ -0,0 +1,104 @@
+package com.example.moviedb.ui.detail
+
+import app.cash.turbine.test
+import com.example.moviedb.core.data.source.Resource
+import com.example.moviedb.core.domain.model.MovieData
+import com.example.moviedb.core.domain.usecase.MovieInteractor
+import com.example.moviedb.core.domain.usecase.MovieUseCase
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+class DetailMovieViewModelTest {
+    private val mainThreadSurrogate = newSingleThreadContext("UI Thread")
+
+    private lateinit var movieUseCase: MovieUseCase
+    private lateinit var detailViewModel: DetailViewModel
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+
+        movieUseCase = Mockito.mock(MovieInteractor::class.java)
+        detailViewModel = DetailViewModel(movieUseCase)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun `should success get detail movie`() = runTest {
+        Mockito.`when`(movieUseCase.getDetailMovie(1)).thenReturn(
+            flow {
+                emit(Resource.Loading())
+                emit(
+                    Resource.Success(
+                        MovieData(
+                            id = 1,
+                            title = "Meg 2: The Trench",
+                            genres = "",
+                            overview = "Some overview of Meg 2: The Trench",
+                            releaseDate = "2023-08-02",
+                            posterPath = "/FQHtuf2zc8suMFE28RyvFt3FJN.jpg"
+                        )
+                    )
+                )
+            }
+        )
+
+        val expected = DetailUIState.Loaded(
+            MovieData(
+                id = 1,
+                title = "Meg 2: The Trench",
+                genres = "",
+                overview = "Some overview of Meg 2: The Trench",
+                releaseDate = "2023-08-02",
+                posterPath = "/FQHtuf2zc8suMFE28RyvFt3FJN.jpg"
+            )
+        )
+
+        detailViewModel.getDetailMovie(1)
+
+        detailViewModel.uiState.test {
+            Assert.assertEquals(DetailUIState.Loading, awaitItem())
+            Assert.assertEquals(expected, awaitItem())
+        }
+
+        Mockito.verify(movieUseCase, Mockito.times(1)).getDetailMovie(1)
+    }
+
+    @Test
+    fun `should error get detail movie`() = runTest {
+        Mockito.`when`(movieUseCase.getDetailMovie(1)).thenReturn(
+            flow {
+                emit(Resource.Loading())
+                emit(Resource.Error(404, "not found"))
+            }
+        )
+
+        val expected = DetailUIState.OnError("not found")
+
+        detailViewModel.getDetailMovie(1)
+
+        detailViewModel.uiState.test {
+            Assert.assertEquals(DetailUIState.Loading, awaitItem())
+            Assert.assertEquals(expected, awaitItem())
+        }
+
+        Mockito.verify(movieUseCase, Mockito.times(1)).getDetailMovie(1)
+    }
+}
---
+++ app/src/test/java/com/example/moviedb/ui/main/MainViewModelTest.kt
@@ -0,0 +1,166 @@
+package com.example.moviedb.ui.main
+
+import app.cash.turbine.test
+import com.example.moviedb.core.data.source.Resource
+import com.example.moviedb.core.domain.model.MovieData
+import com.example.moviedb.core.domain.usecase.MovieInteractor
+import com.example.moviedb.core.domain.usecase.MovieUseCase
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+
+@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+class MainViewModelTest {
+    private val mainThreadSurrogate = newSingleThreadContext("UI Thread")
+
+    private lateinit var movieUseCase: MovieUseCase
+    private lateinit var mainViewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+
+        movieUseCase = mock(MovieInteractor::class.java)
+        mainViewModel = MainViewModel(movieUseCase)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun `should success get now playing movies`() = runTest {
+        `when`(movieUseCase.getNowPlayingMovies()).thenReturn(
+            flow {
+                emit(Resource.Loading())
+                emit(
+                    Resource.Success(
+                        listOf(
+                            MovieData(
+                                id = 1,
+                                title = "Meg 2: The Trench",
+                                genres = "",
+                                overview = "Some overview of Meg 2: The Trench",
+                                releaseDate = "2023-08-02",
+                                posterPath = "/FQHtuf2zc8suMFE28RyvFt3FJN.jpg"
+                            ),
+                            MovieData(
+                                id = 2,
+                                title = "Barbie",
+                                genres = "",
+                                overview = "Some overview of Barbie",
+                                releaseDate = "2023-07-19",
+                                posterPath = "/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg"
+                            ),
+                            MovieData(
+                                id = 3,
+                                title = "Elemental",
+                                genres = "",
+                                overview = "Some overview of Elemental",
+                                releaseDate = "2023-06-14",
+                                posterPath = "/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg"
+                            )
+                        )
+                    )
+                )
+            }
+        )
+
+        val expected = MainUIState.ShowMovies(
+            listOf(
+                MovieData(
+                    id = 1,
+                    title = "Meg 2: The Trench",
+                    genres = "",
+                    overview = "Some overview of Meg 2: The Trench",
+                    releaseDate = "2023-08-02",
+                    posterPath = "/FQHtuf2zc8suMFE28RyvFt3FJN.jpg"
+                ),
+                MovieData(
+                    id = 2,
+                    title = "Barbie",
+                    genres = "",
+                    overview = "Some overview of Barbie",
+                    releaseDate = "2023-07-19",
+                    posterPath = "/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg"
+                ),
+                MovieData(
+                    id = 3,
+                    title = "Elemental",
+                    genres = "",
+                    overview = "Some overview of Elemental",
+                    releaseDate = "2023-06-14",
+                    posterPath = "/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg"
+                )
+            )
+        )
+
+        mainViewModel.getNowPlayingMovies()
+
+        mainViewModel.uiState.test {
+            assertEquals(MainUIState.Loading, awaitItem())
+            assertEquals(expected, awaitItem())
+        }
+
+        verify(movieUseCase, times(1)).getNowPlayingMovies()
+    }
+
+    @Test
+    fun `should success get now playing movies with empty value`() = runTest {
+        Mockito.`when`(movieUseCase.getNowPlayingMovies()).thenReturn(
+            flow {
+                emit(Resource.Loading())
+                emit(Resource.Success(listOf()))
+            }
+        )
+
+        val expected = MainUIState.Empty
+
+        mainViewModel.getNowPlayingMovies()
+
+        mainViewModel.uiState.test {
+            Assert.assertEquals(MainUIState.Loading, awaitItem())
+            Assert.assertEquals(expected, awaitItem())
+        }
+
+        Mockito.verify(movieUseCase, Mockito.times(1)).getNowPlayingMovies()
+    }
+
+    @Test
+    fun `should error get now playing movies`() = runTest {
+        Mockito.`when`(movieUseCase.getNowPlayingMovies()).thenReturn(
+            flow {
+                emit(Resource.Loading())
+                emit(Resource.Error(401, "auth error"))
+            }
+        )
+
+        val expected = MainUIState.OnError("auth error")
+
+        mainViewModel.getNowPlayingMovies()
+
+        mainViewModel.uiState.test {
+            Assert.assertEquals(MainUIState.Loading, awaitItem())
+            Assert.assertEquals(expected, awaitItem())
+        }
+
+        Mockito.verify(movieUseCase, Mockito.times(1)).getNowPlayingMovies()
+    }
+}
